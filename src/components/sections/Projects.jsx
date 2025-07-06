773722d38859339a8677cbebf37ee6b7e2c46c57
Priya Mahato <priyamahato1950@gmail.com>
2025-07-06 21:20:50 +0530

    Added new project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -102,6 +102,38 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">TradeHive</h3>
+              <p className="text-gray-400 mb-4">
+                Developed a full-stack trading app inspired from Zerodha with
+                user authentication, live order execution, portfolio tracking,
+                and holdings management. Used Node.js, Express, MongoDB
+                (Mongoose), and React.js with modular components and state
+                management. Followed RESTful APIs and MVC architecture.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React.js", "Node.js", "MongoDB", "Bcrypt", "JWT"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://tradehive-frontend.onrender.com"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →{" "}
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </RevealOnScroll>
